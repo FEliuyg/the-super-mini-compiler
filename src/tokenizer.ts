fdde0af34a05b5ff7316f823ee867282ad74f72b
xiaoyao <15558110117@163.com>
2024-09-22 10:15:14 +0800

    feat: complete tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,3 +1,91 @@
-export function tokenizer() {
-  return '';
+export enum TokenTypes {
+  Number = 'number',
+  Paren = 'paren',
+  Name = 'name',
+  String = 'string',
+}
+
+export interface Token {
+  type: TokenTypes;
+  value: string;
+}
+
+export function tokenizer(code: string): Token[] {
+  let current = 0;
+  let tokens: Token[] = [];
+
+  while (current < code.length) {
+    let char = code[current];
+    if (char === '(') {
+      tokens.push({
+        type: TokenTypes.Paren,
+        value: char,
+      });
+      current++;
+      continue;
+    }
+
+    if (char === ')') {
+      tokens.push({
+        type: TokenTypes.Paren,
+        value: char,
+      });
+      current++;
+      continue;
+    }
+
+    const LETTERS = /[a-z]/i;
+    if (LETTERS.test(char)) {
+      let value = '';
+      // /[0-9]/.test(undefined) => false， /[a-z]/i.test(undefined) => true，因为有隐式转化 undefined => 'undefined'
+      while (char && LETTERS.test(char)) {
+        value += char;
+        char = code[++current];
+      }
+      tokens.push({
+        type: TokenTypes.Name,
+        value,
+      });
+      continue;
+    }
+
+    const NUMBER = /[0-9]/;
+    if (NUMBER.test(char)) {
+      let value = '';
+      while (NUMBER.test(char)) {
+        value += char;
+        char = code[++current];
+      }
+      tokens.push({
+        type: TokenTypes.Number,
+        value,
+      });
+      continue;
+    }
+
+    const WHITESPACE = /\s/;
+    if (WHITESPACE.test(char)) {
+      current++;
+      continue;
+    }
+
+    if (char === '"') {
+      let value = '';
+      char = code[++current];
+      while (char && char !== '"') {
+        value += char;
+        char = code[++current];
+      }
+      tokens.push({
+        type: TokenTypes.String,
+        value,
+      });
+      current++;
+      continue;
+    }
+
+    throw new Error('Unexpected character');
+  }
+
+  return tokens;
 }
--- test/tokenizer.test.ts
+++ test/tokenizer.test.ts
@@ -1,1 +1,68 @@
 import { test, expect } from 'vitest';
+import { tokenizer } from '../src/tokenizer';
+
+test('tokenizer', () => {
+  const code = '(add 2 (subtract 4 2))';
+
+  const tokens = [
+    { type: 'paren', value: '(' },
+    { type: 'name', value: 'add' },
+    { type: 'number', value: '2' },
+    { type: 'paren', value: '(' },
+    { type: 'name', value: 'subtract' },
+    { type: 'number', value: '4' },
+    { type: 'number', value: '2' },
+    { type: 'paren', value: ')' },
+    { type: 'paren', value: ')' },
+  ];
+
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test('left paren', () => {
+  const code = '(';
+  const tokens = [{ type: 'paren', value: '(' }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test('name', () => {
+  const code = 'add';
+  const tokens = [{ type: 'name', value: 'add' }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test('number', () => {
+  const code = '123';
+  const tokens = [{ type: 'number', value: '123' }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test('right paren', () => {
+  const code = ')';
+  const tokens = [{ type: 'paren', value: ')' }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test('(add 2 3)', () => {
+  const code = '(add 2 3)';
+  const tokens = [
+    { type: 'paren', value: '(' },
+    { type: 'name', value: 'add' },
+    { type: 'number', value: '2' },
+    { type: 'number', value: '3' },
+    { type: 'paren', value: ')' },
+  ];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test('(concat "ab" "bc")', () => {
+  const code = '(concat "ab" "bc")';
+  const tokens = [
+    { type: 'paren', value: '(' },
+    { type: 'name', value: 'concat' },
+    { type: 'string', value: 'ab' },
+    { type: 'string', value: 'bc' },
+    { type: 'paren', value: ')' },
+  ];
+  expect(tokenizer(code)).toEqual(tokens);
+});
